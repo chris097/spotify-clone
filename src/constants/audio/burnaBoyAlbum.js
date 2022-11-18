850cd9427edb1e7c74c730b30974e693c8ec84da
chris097 <chrisfidel.international@gmail.com>
2022-11-18 09:20:17 +0100

    uploading songs

---
+++ src/constants/audio/burnaBoyAlbum.js
@@ -0,0 +1,5 @@
+const BURNA_BOY_ALBUM = {
+    SONG_LISTS: {},
+    SONG_TITLE: {},
+    ALBUM_NAME: {}
+}
---
+++ src/public/audio/index.js
@@ -0,0 +1,5 @@
+
+
+const burna_boy_songs = [
+    {id: 1, src: 'Burna_Boy_-_Cloak_Dagger_ft_J_Hus_042jam.com.mp3'}
+]
