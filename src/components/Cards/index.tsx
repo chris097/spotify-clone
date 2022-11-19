51844c92bcdd8b2c42c072194bd2003d68e04530
chris097 <chrisfidel.international@gmail.com>
2022-11-19 01:03:36 +0100

    playlist architecture

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -15,10 +15,10 @@ const Card: React.FC<Props> = (props) => {
             <div className='w-full'>
               <div className="transform-gpu flex justify-end w-full space-y-3 text-xl group-hover:translate-y-0 translate-y-4 pb-10 transform transition duration-300 ease-in-out">    
               <div className='w-full flex justify-end'>
-                <div className='bg-primarygreen flex justify-center w-14 h-14 -mt-32 mr-6 absolute rounded-full items-center'>
+                <button type="button" className='bg-primarygreen flex justify-center w-14 h-14 -mt-32 mr-6 absolute rounded-full items-center'>
                   <svg role="img" height="24" width="24" viewBox="0 0 24 24"><path d="M7.05 3.606l13.49 7.788a.7.7 0 010 1.212L7.05 20.394A.7.7 0 016 19.788V4.212a.7.7 0 011.05-.606z"></path></svg>
                 {/* <svg height="24" width="24" viewBox="0 0 24 24"><path d="M5.7 3a.7.7 0 00-.7.7v16.6a.7.7 0 00.7.7h2.6a.7.7 0 00.7-.7V3.7a.7.7 0 00-.7-.7H5.7zm10 0a.7.7 0 00-.7.7v16.6a.7.7 0 00.7.7h2.6a.7.7 0 00.7-.7V3.7a.7.7 0 00-.7-.7h-2.6z"></path></svg> */}
-                        </div>
+                        </button>
               </div>
                     </div>
                 </div>
