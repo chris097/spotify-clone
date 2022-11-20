56f96e16f2de57bd876b01ab427421395b168822
chris097 <chrisfidel.international@gmail.com>
2022-11-21 00:04:02 +0100

    playlist architecture

--- src/playlist/afrobeat/burnaboy.js
+++ src/playlist/afrobeat/burnaboy.js
@@ -1,5 +1,5 @@
 import Cloak_Dagger from '../../public/audio/Burna_Boy_-_Cloak_Dagger_ft_J_Hus_042jam.com.mp3';
 
 export const songs = [
-    { id: 1, title: "Cloak Dagger", url: Cloak_Dagger, artist: "Burna_Boy ft J_Hus" }
+    { id: 1, title: "Cloak Dagger", url: Cloak_Dagger, artist: "Burna_Boy ft J_Hus", album: "Love Damini"}
 ];
