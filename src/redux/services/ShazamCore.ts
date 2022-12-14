02a1753877cf239b12c5ecec1f45c1f700685d53
chris097 <chrisfidel.international@gmail.com>
2022-12-14 06:20:56 +0100

    bug fix

--- src/redux/services/ShazamCore.ts
+++ src/redux/services/ShazamCore.ts
@@ -1,11 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const { REACT_X_RAPIDAPI_KEY } = process.env;
+console.log(REACT_X_RAPIDAPI_KEY)
+
 export const shazamCoreApi = createApi({
     reducerPath: "shazamCoreApi",
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', '46dc162c15mshdab909459396508p146f92jsn0dfe3a12bef1')
+            headers.set('X-RapidAPI-Key', `46dc162c15mshdab909459396508p146f92jsn0dfe3a12bef1`)
 
             return headers;
         },
