26dc28ac5c9cdf93b147335385900383ad7627e4
chris097 <chrisfidel.international@gmail.com>
2022-12-16 09:12:15 +0100

    feature top card

---
+++ src/components/Cards/TopCard.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const TopCard = () => {
+  return (
+      <div>
+      <div className='flex gap-7'>
+          <div className='w-full h-[60px] border border-white bg-[#727272]'>
+              Do something here...
+        </div>
+          <div className='w-full h-[60px] border border-white'>
+              Do something here...
+        </div>
+          <div className='w-full h-[60px] border border-white'>
+              Do something here...
+        </div>
+    </div>
+      <div className='flex gap-7 mt-5'>
+          <div className='w-full h-[60px] border border-white bg-[#727272]'>
+              Do something here...
+        </div>
+          <div className='w-full h-[60px] border border-white'>
+              Do something here...
+        </div>
+          <div className='w-full h-[60px] border border-white'>
+              Do something here...
+        </div>
+    </div>
+      </div>
+  )
+}
+
+export default TopCard
